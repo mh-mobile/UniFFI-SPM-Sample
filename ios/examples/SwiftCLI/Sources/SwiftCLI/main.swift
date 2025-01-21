154028d50ea406ff697828bd8f04eff7067dea35
mh-mobile <mh.mobiler@gmail.com>
2025-01-21 17:04:21 +0900

    SwiftCLIのサンプルを修正

--- ios/examples/SwiftCLI/Sources/SwiftCLI/main.swift
+++ ios/examples/SwiftCLI/Sources/SwiftCLI/main.swift
@@ -3,3 +3,8 @@ import Mobile
 // say_hiメソッドを呼び出し
 let message = sayHi()
 print(message)
+
+// calculatorを使って計算
+let calculator = Calculator(initialValue: 0)
+calculator.add(x: 7)
+print(calculator.getValue())
