8cc99dc08f2a9d7c9262e7299cda1d2c792c60a7
mh-mobile <mh.mobiler@gmail.com>
2025-05-29 06:19:13 +0900

    iOS SimulatorビルドにてIntel Mac用のx86_64アーキテクチャサポートを追加
    - シェバンの構文エラーを修正 (!/bin/bash -> #!/bin/bash)
    - Intel Mac用のiOS Simulatorターゲット(x86_64-apple-ios)を追加
    - lipoを使用してiOS Simulator用のユニバーサルバイナリを作成
    - XCFrameworkでユニバーサルシミュレータライブラリを使用するよう更新

--- build-ios.sh
+++ build-ios.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Exit on error
 set -e
@@ -13,6 +13,7 @@ cargo run --bin uniffi-bindgen generate --library ./target/release/libmobile.dyl
 for TARGET in \
        aarch64-apple-ios \
        aarch64-apple-ios-sim \
+       x86_64-apple-ios \
        aarch64-apple-darwin
 do
    rustup target add $TARGET
@@ -24,11 +25,18 @@ cp ./bindings/mobile.swift ./ios/src/Mobile/
 cp ./bindings/mobileFFI.h ./ios/src/Mobile/
 cp ./bindings/mobileFFI.modulemap ./bindings/module.modulemap
 
+# Create fat library for iOS Simulator (combining arm64 and x86_64)
+mkdir -p ./target/ios-simulator-universal/release
+lipo -create \
+   ./target/aarch64-apple-ios-sim/release/libmobile.a \
+   ./target/x86_64-apple-ios/release/libmobile.a \
+   -output ./target/ios-simulator-universal/release/libmobile.a
+
 # Create XCFramework
 rm -rf "ios/bin/MobileCore.xcframework"
 xcodebuild -create-xcframework \
    -library  ./target/aarch64-apple-ios/release/libmobile.a -headers ./bindings \
-   -library  ./target/aarch64-apple-ios-sim/release/libmobile.a -headers ./bindings \
+   -library  ./target/ios-simulator-universal/release/libmobile.a -headers ./bindings \
    -library  ./target/aarch64-apple-darwin/release/libmobile.a -headers ./bindings \
    -output "ios/bin/MobileCore.xcframework"
 
